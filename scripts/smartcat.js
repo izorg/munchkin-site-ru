02e0416f65d0c2c3fd4dd1f3cc8438e69ec84918
Viacheslav Zavoruev <izorg@yandex.ru>
2018-07-08 12:18:42 +0200

    Update Smartcat API

--- scripts/smartcat.js
+++ scripts/smartcat.js
@@ -1,11 +1,11 @@
+/* eslint-disable no-console */
 const axios = require('axios');
-const exec = require('child_process').exec;
-const fs = require('fs');
+const { exec } = require('child_process');
 
+// eslint-disable-next-line import/no-unresolved
 const config = require('../config.json').smartcat;
 
-const projectId = config.projectId;
-const documentId = config.documentId;
+const { documentId, projectId } = config;
 
 axios.defaults.baseURL = 'https://smartcat.ai/api/integration/v1';
 axios.defaults.auth = {
@@ -13,48 +13,66 @@ axios.defaults.auth = {
   password: config.key,
 };
 
-function exportDocument(document) {
-  return axios.post('/document/export', undefined, {
+const exportDocument = async (document) => {
+  const { data } = await axios.post('/document/export', undefined, {
     params: {
       documentIds: [document.id],
     },
-  }).then(({ data }) => data);
-}
-
-const deleteDocuments = documentIds => axios.delete('/document', { params: { documentIds } });
-
-const getDocuments = () => axios.get(`/project/${projectId}`).then(({ data: { documents } }) => documents);
-
-function getTaskResult(taskId, language, resolve) {
-  console.log('getTaskResult');
-  axios.get(`/document/export/${taskId}`).then(({ data, status }) => {
-    console.log('status', status);
-    if (status === 200 && data) {
-      resolve(data);
-    } else if (status === 204) {
-      setTimeout(() => {
-        getTaskResult(taskId, language, resolve);
-      }, 1000);
-    }
   });
+
+  return data;
+};
+
+const deleteDocuments = async (documentIds) =>
+  axios.delete('/document', { params: { documentIds } });
+
+const getDocuments = async () => {
+  const {
+    data: { documents },
+  } = await axios.get(`/project/${projectId}`);
+
+  return documents;
+};
+
+const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+function* getTask(taskId) {
+  while (true) {
+    yield axios.get(`/document/export/${taskId}`);
+    yield timeout(1000);
+  }
 }
 
-function getTranslation(taskId, language) {
-  return new Promise(((resolve) => {
-    getTaskResult(taskId, language, resolve);
-  }));
-}
+const getTranslation = async (taskId) => {
+  // eslint-disable-next-line no-restricted-syntax
+  for await (const result of getTask(taskId)) {
+    if (result) {
+      const { data, status } = result;
+
+      if (status === 200 && data) {
+        return data;
+      }
+    }
+  }
+
+  return undefined;
+};
 
 const updateDocument = (filePath) => {
   exec(
-    `curl -X PUT --user ${config.accountId}:${config.key} -F "file=@${filePath}" ${axios.defaults.baseURL}/document/update?documentId=${documentId}`,
+    `curl -X PUT --user ${config.accountId}:${
+      config.key
+    } -F "file=@${filePath}" ${
+      axios.defaults.baseURL
+    }/document/update?documentId=${documentId}`,
     (error, stdout) => {
       if (error) {
         console.log('error', error);
       } else {
         console.log(stdout);
       }
-    });
+    },
+  );
 };
 
 module.exports = {
