fc4b846af8e3867eeb4135fd573a4552c1c85585
Viacheslav Zavoruev <izorg@yandex.ru>
2018-07-10 00:56:32 +0200

    Optimize babel & webpack config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,4 @@
 /* eslint-disable react/jsx-filename-extension */
-import '@babel/polyfill';
 import React from 'react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,44 @@
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-exports.onCreateWebpackConfig = ({ actions, stage }) => {
+exports.onCreateBabelConfig = ({ actions, stage }) => {
+  if (stage === 'develop') {
+    actions.setBabelPlugin({
+      name: 'babel-plugin-react-intl',
+      options: { messagesDir: './messages/' },
+    });
+  }
+};
+
+exports.onCreateWebpackConfig = ({ actions, stage, store }) => {
+  if (stage === 'develop' || stage === 'build-javascript') {
+    const { webpack } = store.getState();
+    const presetEnvOptions =
+      webpack.module.rules[0].use[0].options.presets[0][1];
+
+    Object.assign(presetEnvOptions, {
+      // debug: true,
+      useBuiltIns: 'entry',
+      targets: {
+        android: '4.4',
+        chrome: 49,
+        edge: 14,
+        firefox: 45,
+        ie: 11,
+        safari: 10,
+      },
+    });
+
+    if (stage === 'build-javascript') {
+      webpack.entry.app = ['./src/polyfill.js', webpack.entry.app];
+    }
+
+    if (stage === 'develop') {
+      webpack.entry.commons.unshift('@babel/polyfill');
+    }
+
+    actions.replaceWebpackConfig(webpack);
+  }
+
   if (process.env.STATS && stage === 'build-javascript') {
     actions.setWebpackConfig({
       plugins: [
