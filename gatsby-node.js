34ab2ec29ac8b65ec46f1a4e56367a09dc3f742e
Viacheslav Zavoruev <izorg@yandex.ru>
2019-05-09 07:28:07 +0200

    Fix RHL warning

---
+++ gatsby-node.js
@@ -0,0 +1,10 @@
+exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
+  const config = getConfig();
+
+  if (stage.startsWith('develop') && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react-dom': '@hot-loader/react-dom',
+    };
+  }
+};
