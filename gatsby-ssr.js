2975d72b637e0010d223a67742cb26a3cf8321fa
Viacheslav Zavoruev <izorgster@gmail.com>
2021-04-28 23:59:54 +0200

    MUI upgrade

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,4 @@
 import createEmotionServer from "@emotion/server/create-instance";
-import { ServerStyleSheets } from "@material-ui/core";
 import { renderToString } from "react-dom/server";
 
 import MuiProvider, { cache } from "./src/components/MuiProvider";
@@ -7,16 +6,13 @@ import MuiProvider, { cache } from "./src/components/MuiProvider";
 const emotionServer = createEmotionServer(cache);
 
 export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
-  const sheets = new ServerStyleSheets();
-
-  const bodyHTML = renderToString(sheets.collect(bodyComponent));
+  const bodyHTML = renderToString(bodyComponent);
 
   const styles = emotionServer.extractCritical(bodyHTML);
 
   replaceBodyHTMLString(bodyHTML);
 
   setHeadComponents([
-    sheets.getStyleElement(),
     <style
       key="emotion-style-tag"
       dangerouslySetInnerHTML={{ __html: styles.css }}
